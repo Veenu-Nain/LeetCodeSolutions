56077f72ce125b341ffc77223909db13d9fd44fd
Veenu Nain <veenunain2824@gmail.com>
2025-01-02 18:21:11 +0530

    ans2596

---
+++ que2596.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool helper(vector<vector<int>>& grid,int r,int c,int n,int expVal){
+        if(r<0 || c<0 || r>=n || c>=n || grid[r][c] != expVal){
+            return false;
+        }
+        if(expVal == n*n -1){
+            return true;
+        }
+        //8 possible moves
+        int ans1 = helper(grid,r-2,c+1,n,expVal+1);
+        int ans2 = helper(grid,r-1,c+2,n,expVal+1);
+        int ans3 = helper(grid,r+1,c+2,n,expVal+1);
+        int ans4 = helper(grid,r+2,c+1,n,expVal+1);
+        int ans5 = helper(grid,r+2,c-1,n,expVal+1);
+        int ans6 = helper(grid,r+1,c-2,n,expVal+1);
+        int ans7 = helper(grid,r-1,c-2,n,expVal+1);
+        int ans8 = helper(grid,r-2,c-1,n,expVal+1);
+
+        return ans1 || ans2 || ans3 || ans4 || ans5 || ans6 || ans7 || ans8;
+    }
+    bool checkValidGrid(vector<vector<int>>& grid) {
+       return helper(grid,0,0,grid.size(),0);
+    }
+};
